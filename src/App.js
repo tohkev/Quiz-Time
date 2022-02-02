370031f9a704c51c9e556a63539b6dafdb71c21b
Kevin <tohkev@gmail.com>
2022-02-01 20:40:41 -0800

    added form functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React from 'react';
 import Home from './components/Home.js';
-import Question from './components/Question.js'
+import Question from './components/Question.js';
+import { nanoid } from 'nanoid';
 
 
 export default function App() {
+
+  const [questions, setQuestions] = React.useState([])
+
+  React.useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=5&type=multiple')
+      .then(res => res.json())
+      .then(data => setQuestions(data.results))
+  }, [])
+
+  const questionElements = questions.map(element => {
+    let id = nanoid();
+
+    if (element.type === "boolean") {
+      return [];
+    } else {
+      let choices = element.incorrect_answers;
+      let index = Math.floor(Math.random() * 4);
+      choices.splice(index, 0, element.correct_answer);
+
+      return <Question
+        key={id}
+        id={id}
+        question={element.question}
+        choice1={choices[0]}
+        choice2={choices[1]}
+        choice3={choices[2]}
+        choice4={choices[3]}
+      />
+    }
+  })
+
+  console.log(questions)
+
   return (
     <main>
       {false && <Home />}
       <form className="quiz--block">
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
+        {questionElements}
         <button className="quiz--check-btn">Check Answers</button>
       </form>
     </main>
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import Answer from './Answer.js'
 
-export default function Question() {
+export default function Question(props) {
     return (
         <div className="question--block">
-            <h3 className="question--question">How would one say goodbye in Spanish?</h3>
+            <h3 className="question--question" dangerouslySetInnerHTML={{ __html: props.question }}></h3>
             <div className="question--answer">
                 <div className="question--choice">
-                    <input type="radio" id="choice1" value="choice1" name="question1" className="question--radio" />
-                    <label for="choice1" className="radio-label">Choice 1</label>
+                    <input type="radio" id={`choice1-${props.id}`} value="choice1" name={props.id} className="question--radio" />
+                    <label for={`choice1-${props.id}`} className="radio-label" dangerouslySetInnerHTML={{ __html: props.choice1 }}></label>
                 </div>
                 <div className="question--choice">
-                    <input type="radio" id="choice2" value="choice2" name="question1" className="question--radio" />
-                    <label for="choice2" className="radio-label">Choice 2</label>
+                    <input type="radio" id={`choice2-${props.id}`} value="choice2" name={props.id} className="question--radio" />
+                    <label for={`choice2-${props.id}`} className="radio-label" dangerouslySetInnerHTML={{ __html: props.choice2 }}></label>
                 </div>
                 <div className="question--choice">
-                    <input type="radio" id="choice3" value="choice3" name="question1" className="question--radio" />
-                    <label for="choice3" className="radio-label">Choice 3</label>
+                    <input type="radio" id={`choice3-${props.id}`} value="choice3" name={props.id} className="question--radio" />
+                    <label for={`choice3-${props.id}`} className="radio-label" dangerouslySetInnerHTML={{ __html: props.choice3 }}></label>
                 </div>
                 <div className="question--choice">
-                    <input type="radio" id="choice4" value="choice4" name="question1" className="question--radio" />
-                    <label for="choice4" className="radio-label">Choice 4</label>
+                    <input type="radio" id={`choice4-${props.id}`} value="choice4" name={props.id} className="question--radio" />
+                    <label for={`choice4-${props.id}`} className="radio-label" dangerouslySetInnerHTML={{ __html: props.choice4 }}></label>
                 </div>
             </div>
         </div>

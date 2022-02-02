7d024992f90b58afa8c8c1cf55958d8f2eadffc2
Kevin <tohkev@gmail.com>
2022-02-01 20:55:28 -0800

    fixed name issue

--- src/App.js
+++ src/App.js
@@ -8,21 +8,29 @@ export default function App() {
 
   const [questions, setQuestions] = React.useState([])
 
+  const [quizData, setQuizData] = React.useState({
+    "question-1": null,
+    "question-2": null,
+    "question-3": null,
+    "question-4": null,
+    "question-5": null
+  })
+
   React.useEffect(() => {
     fetch('https://opentdb.com/api.php?amount=5&type=multiple')
       .then(res => res.json())
       .then(data => setQuestions(data.results))
   }, [])
 
-  const questionElements = questions.map(element => {
+  const questionElements = questions.map((element, index) => {
     let id = nanoid();
 
     if (element.type === "boolean") {
       return [];
     } else {
       let choices = element.incorrect_answers;
-      let index = Math.floor(Math.random() * 4);
-      choices.splice(index, 0, element.correct_answer);
+      let place = Math.floor(Math.random() * 4);
+      choices.splice(place, 0, element.correct_answer);
 
       return <Question
         key={id}
@@ -32,6 +40,8 @@ export default function App() {
         choice2={choices[1]}
         choice3={choices[2]}
         choice4={choices[3]}
+        name={`question-${index + 1}`}
+        correct={place}
       />
     }
   })
--- src/components/Question.js
+++ src/components/Question.js
@@ -7,19 +7,19 @@ export default function Question(props) {
             <h3 className="question--question" dangerouslySetInnerHTML={{ __html: props.question }}></h3>
             <div className="question--answer">
                 <div className="question--choice">
-                    <input type="radio" id={`choice1-${props.id}`} value="choice1" name={props.id} className="question--radio" />
+                    <input type="radio" id={`choice1-${props.id}`} value="choice1" name={props.name} className="question--radio" />
                     <label for={`choice1-${props.id}`} className="radio-label" dangerouslySetInnerHTML={{ __html: props.choice1 }}></label>
                 </div>
                 <div className="question--choice">
-                    <input type="radio" id={`choice2-${props.id}`} value="choice2" name={props.id} className="question--radio" />
+                    <input type="radio" id={`choice2-${props.id}`} value="choice2" name={props.name} className="question--radio" />
                     <label for={`choice2-${props.id}`} className="radio-label" dangerouslySetInnerHTML={{ __html: props.choice2 }}></label>
                 </div>
                 <div className="question--choice">
-                    <input type="radio" id={`choice3-${props.id}`} value="choice3" name={props.id} className="question--radio" />
+                    <input type="radio" id={`choice3-${props.id}`} value="choice3" name={props.name} className="question--radio" />
                     <label for={`choice3-${props.id}`} className="radio-label" dangerouslySetInnerHTML={{ __html: props.choice3 }}></label>
                 </div>
                 <div className="question--choice">
-                    <input type="radio" id={`choice4-${props.id}`} value="choice4" name={props.id} className="question--radio" />
+                    <input type="radio" id={`choice4-${props.id}`} value="choice4" name={props.name} className="question--radio" />
                     <label for={`choice4-${props.id}`} className="radio-label" dangerouslySetInnerHTML={{ __html: props.choice4 }}></label>
                 </div>
             </div>

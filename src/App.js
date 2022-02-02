dcaaa8522a14be5ea414668c2ec830a3834581c0
Kevin <tohkev@gmail.com>
2022-02-01 22:33:31 -0800

    implemented react radio inputs

--- src/App.js
+++ src/App.js
@@ -9,44 +9,76 @@ export default function App() {
   const [questions, setQuestions] = React.useState([])
 
   const [quizData, setQuizData] = React.useState({
-    "question-1": null,
-    "question-2": null,
-    "question-3": null,
-    "question-4": null,
-    "question-5": null
+    "question1": "",
+    "question2": "",
+    "question3": "",
+    "question4": "",
+    "question5": ""
   })
+
+  console.log(quizData);
+
+  console.log(questions);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    console.log(event.target)
+    setQuizData(prevData => {
+      return {
+        ...prevData,
+        [name]: value
+      }
+    })
+  }
 
   React.useEffect(() => {
     fetch('https://opentdb.com/api.php?amount=5&type=multiple')
       .then(res => res.json())
-      .then(data => setQuestions(data.results))
+      .then(data => setQuestions(getChoices(data.results)))
   }, [])
 
-  const questionElements = questions.map((element, index) => {
-    let id = nanoid();
 
-    if (element.type === "boolean") {
-      return [];
-    } else {
+  function getChoices(data) {
+    return data.map((element, index) => {
+
       let choices = element.incorrect_answers;
       let place = Math.floor(Math.random() * 4);
       choices.splice(place, 0, element.correct_answer);
 
+      return {
+        id: nanoid(),
+        type: element.type,
+        question: element.question,
+        correctValue: element["correct_answer"],
+        correctPlace: place,
+        choices: choices
+      }
+    })
+  }
+
+  const questionElements = questions.map((element, index) => {
+
+    //implementing boolean questions in the future
+    if (element.type === "boolean") {
+      return [];
+
+      //multiple choice questions
+    } else {
       return <Question
-        key={id}
-        id={id}
+        key={element.id}
+        id={element.id}
         question={element.question}
-        choice1={choices[0]}
-        choice2={choices[1]}
-        choice3={choices[2]}
-        choice4={choices[3]}
-        name={`question-${index + 1}`}
-        correct={place}
+        choice1={element.choices[0]}
+        choice2={element.choices[1]}
+        choice3={element.choices[2]}
+        choice4={element.choices[3]}
+        name={`question${index + 1}`}
+        correct={`choice${element.correctPlace + 1}`}
+        handleChange={handleChange}
+        quizData={quizData}
       />
     }
   })
-
-  console.log(questions)
 
   return (
     <main>
--- src/components/Question.js
+++ src/components/Question.js
@@ -7,19 +7,19 @@ export default function Question(props) {
             <h3 className="question--question" dangerouslySetInnerHTML={{ __html: props.question }}></h3>
             <div className="question--answer">
                 <div className="question--choice">
-                    <input type="radio" id={`choice1-${props.id}`} value="choice1" name={props.name} className="question--radio" />
+                    <input type="radio" id={`choice1-${props.id}`} value="choice1" name={props.name} className="question--radio" onChange={props.handleChange} checked={props.quizData[props.name] === "choice1"} />
                     <label for={`choice1-${props.id}`} className="radio-label" dangerouslySetInnerHTML={{ __html: props.choice1 }}></label>
                 </div>
                 <div className="question--choice">
-                    <input type="radio" id={`choice2-${props.id}`} value="choice2" name={props.name} className="question--radio" />
+                    <input type="radio" id={`choice2-${props.id}`} value="choice2" name={props.name} className="question--radio" onChange={props.handleChange} checked={props.quizData[props.name] === "choice2"} />
                     <label for={`choice2-${props.id}`} className="radio-label" dangerouslySetInnerHTML={{ __html: props.choice2 }}></label>
                 </div>
                 <div className="question--choice">
-                    <input type="radio" id={`choice3-${props.id}`} value="choice3" name={props.name} className="question--radio" />
+                    <input type="radio" id={`choice3-${props.id}`} value="choice3" name={props.name} className="question--radio" onChange={props.handleChange} checked={props.quizData[props.name] === "choice3"} />
                     <label for={`choice3-${props.id}`} className="radio-label" dangerouslySetInnerHTML={{ __html: props.choice3 }}></label>
                 </div>
                 <div className="question--choice">
-                    <input type="radio" id={`choice4-${props.id}`} value="choice4" name={props.name} className="question--radio" />
+                    <input type="radio" id={`choice4-${props.id}`} value="choice4" name={props.name} className="question--radio" onChange={props.handleChange} checked={props.quizData[props.name] === "choice4"} />
                     <label for={`choice4-${props.id}`} className="radio-label" dangerouslySetInnerHTML={{ __html: props.choice4 }}></label>
                 </div>
             </div>
